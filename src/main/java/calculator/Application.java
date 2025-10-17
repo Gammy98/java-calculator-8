e65e953ef90e31443ddf0772db1179cc246c8fd4
Gammy98 <dnqorgh0410@naver.com>
2025-10-17 17:56:24 +0900

    feat : 빈 문자열 또는 null 입력 시 0반환 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,13 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        if (input == null || input.isEmpty()) { //Consol이 빈문자열을 반환하거나 null 반환
+            System.out.println("결과 : 0");
+        }
+        else {
+            // TODO : 4번기능 구현
+            // TODO : 기능 마저 구현후 중복 방지로 출력 위치변경 필요해보임
+        }
     }
 }
