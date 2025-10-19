4bb606e060735996cb2f9e5dd60322fe3cf465df
Gammy98 <dnqorgh0410@naver.com>
2025-10-19 10:08:09 +0900

    feat : 커스텀 구분자 기능 구현

--- src/main/java/calculator/Separating.java
+++ src/main/java/calculator/Separating.java
@@ -1,22 +1,46 @@
 package calculator;
 
+import java.util.*;
+
 //MVC 에서 MODEL역할이고 Application이 Controller 그리고 Console.readLine 과 System.out과 같은 입출력이 View!!
 public class Separating {
     public int add(String input) {
-        int result = 0; // 0으로 초기화
 
-        // 3번 + 4번 기능 로직 (main에서 그대로 가져옴)
-        // "만약 text가 null도 아니고, 비어있지도 않다면 계산"
-        if (input != null && !input.isEmpty()) {
-            String[] strNum = input.split(",|:"); // 4번 기능
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
 
-            for (String snum : strNum) {
-                // TODO : 음수거나 숫자가 아닌 문자가 있으면 예외처리 필요
-                if (!snum.isEmpty()) {
-                    result += Integer.parseInt(snum);
-                }
+        String separator = ",:"; //기본구분자 설정
+        String text = input;
+
+        if (input.startsWith("//")) {
+            int sepindex = input.indexOf("\n");
+            if (sepindex == -1) {
+                throw new IllegalArgumentException("커스텀 구분자 형식이 올바르지 않습니다.");
+            }
+            String customseparator = input.substring(2, sepindex);
+
+            if (!customseparator.isEmpty()) {
+                separator += customseparator;
+            }
+            text = input.substring(sepindex + 1);
+        }
+
+        StringTokenizer st = new StringTokenizer(text, separator);
+        List<String> numberList = new ArrayList<>();
+        while (st.hasMoreTokens()) {
+            numberList.add(st.nextToken());
+        }
+        int result = 0;
+
+
+        for (String snum : numberList) {
+            // TODO : 음수거나 숫자가 아닌 문자가 있으면 예외처리 필요
+            if (!snum.isEmpty()) {
+                result += Integer.parseInt(snum);
             }
         }
+
         return result; // 0 또는 계산된 sum을 반환
     }
 }
